76bd9946170d73fc777a05150ec90971b0627ef4
Diar suma <diar@sempoa.id>
2025-09-16 17:00:45 +0700

    fix variable

--- src/config/rabbitmq.config.ts
+++ src/config/rabbitmq.config.ts
@@ -2,6 +2,6 @@ export default () => ({
   rabbitmq: {
     url: process.env.RABBITMQ_URL || 'amqp://localhost:5672',
     refundQueue: process.env.RABBITMQ_QUEUE || 'bridgepay-refund',
-    coreQueue: process.env.RABBITMQ_QUEUE_CORE || 'bridgepay-core'
+    coreQueue: process.env.RABBITMQ_CORE_QUEUE || 'bridgepay-core'
   },
 });
