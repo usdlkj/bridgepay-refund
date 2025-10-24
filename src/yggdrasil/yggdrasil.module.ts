193074e6c56eb28c01730a7ab75c31b514d3cbf2
Diar suma <diar@sempoa.id>
2025-10-24 16:58:30 +0700

    adjustment coding but not tested. please dont pull until next commit.

--- src/yggdrasil/yggdrasil.module.ts
+++ src/yggdrasil/yggdrasil.module.ts
@@ -5,6 +5,6 @@ import { RefundModule } from './refund/refund.module';
 @Module({
   providers: [YggdrasilService],
   exports: [YggdrasilService],
-  imports: [ RefundModule],
+  imports: [RefundModule],
 })
 export class YggdrasilModule {}
